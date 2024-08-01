96e6fe622534fa2124561b33bc8d4bf6e3233ff7
Gülseren Zamir <86431567+gulseren27@users.noreply.github.com>
2024-08-01 15:34:58 +0300

    ADASO-AgreementApp
    ADASO-AgreementApp

---
+++ ADASO-AgreementApp/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ADASO_AgreementApp.Models.Entity;
+
+namespace ADASO_AgreementApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        ADASOEntities1 db = new ADASOEntities1();
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ADASO-AgreementApp/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ADASO_AgreementApp.Models.Entity;
+
+namespace ADASO_AgreementApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        ADASOEntities1 db = new ADASOEntities1();
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
