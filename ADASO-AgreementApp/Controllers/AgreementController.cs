cec5ce413847ec186632768d9f1b818abb9ec7f1
Avni Can Dördü <avnicandordu@gmail.com>
2024-08-06 19:21:46 +0300

    admin layout ve index hazır

---
+++ ADASO-AgreementApp/Controllers/AgreementController.cs
@@ -0,0 +1,19 @@
+﻿using ADASO_AgreementApp.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ADASO_AgreementApp.Controllers
+{
+    public class AgreementController : Controller
+    {
+        // GET: Agreement
+        ADASOEntities1 db = new ADASOEntities1 ();
+        public ActionResult Agreements()
+        {
+            return View();
+        }
+    }
+}
