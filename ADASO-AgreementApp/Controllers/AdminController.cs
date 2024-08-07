a0ecf0e616504c0d7637e7fa527b98fe0f2cada2
gaye00 <gayecengiz@outlook.com>
2024-08-07 12:17:11 +0300

    admin and agree controller

--- ADASO-AgreementApp/Controllers/AdminController.cs
+++ ADASO-AgreementApp/Controllers/AdminController.cs
@@ -17,7 +17,29 @@ namespace ADASO_AgreementApp.Controllers
         [Authorize]
         public ActionResult Index()
         {
-            return View();
+            var list = db.TBLAdmin.ToList();
+            return View(list);
+        }
+
+        public ActionResult Guncelle(TBLAdmin p)
+        {
+            var admin = db.TBLAdmin.Find(p.AdminID);
+            admin.AdminName = p.AdminName;
+            admin.AdminSurname = p.AdminSurname;
+            admin.Tel = p.Tel;
+            admin.AdminMail = p.AdminMail;
+            admin.TC = p.TC;
+            admin.Image = p.Image;
+            //admin.AgreementNo = p.AgreementNo;
+            var sozlesme = db.TBLAgreement.Where(m => m.AgreementNo == p.TBLAgreement.AgreementNo).FirstOrDefault();
+            admin.AgreementNo = sozlesme.AgreementNo;
+            admin.Role = p.Role;
+            admin.Password = p.Password;
+
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+
         }
     }
 }
--- ADASO-AgreementApp/Controllers/AgreementController.cs
+++ ADASO-AgreementApp/Controllers/AgreementController.cs
@@ -13,7 +13,28 @@ namespace ADASO_AgreementApp.Controllers
         ADASOEntities2 db = new ADASOEntities2();
         public ActionResult Index()
         {
-            return View();
+            var list = db.TBLAgreement.ToList();
+            return View(list);
+        }
+
+        public ActionResult Guncelle(TBLAgreement p)
+        {
+            var soz = db.TBLAgreement.Find(p.AgreementNo);
+            soz.AgrTitle = p.AgrTitle;
+            soz.AgrContent = p.AgrContent;
+            soz.AgrFinishDate = p.AgrFinishDate;
+            soz.AgrStartDate = p.AgrStartDate;
+            soz.Image = p.Image;
+            soz.CompanyName = p.CompanyName;
+            soz.AdminID = p.AdminID;
+            //var admin = db.TBLAdmin.Where(m => m.AdminID == p.TBLAdmin.AdminID).FirstOrDefault();
+            //soz.AdminID = admin.AdminID;
+            soz.Email = p.Email;
+
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+
         }
     }
 }
